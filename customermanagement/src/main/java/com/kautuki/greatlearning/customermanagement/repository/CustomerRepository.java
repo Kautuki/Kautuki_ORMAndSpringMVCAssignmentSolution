4cccf5ed1f934703ba5e024d22eca0b8079aedfa
Kautuki <80846505+Kautuki@users.noreply.github.com>
2022-11-24 15:14:45 +0530

    Add files via upload

---
+++ customermanagement/src/main/java/com/kautuki/greatlearning/customermanagement/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.kautuki.greatlearning.customermanagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kautuki.greatlearning.customermanagement.model.Customer;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
